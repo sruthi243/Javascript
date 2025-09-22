c9154308320c2d117dac477a74730984d19c92d4
Sruthi <sruthisirigiri93@gmail.com>
2025-09-22 12:25:54 +0530

    change

---
+++ function.js
@@ -0,0 +1,26 @@
+//function
+//code resuability
+function add(){
+    var a=57;
+    var b=78;
+    // document.write(a+b);
+    console.log(a+b);
+    
+
+}
+add();
+
+//way 2 of using functions(Es6)
+const sub=()=>{
+    // function sub(){
+    var a=57;
+    var b=78;
+    // document.write(a+b);
+    console.log(a-b);
+}
+// }
+sub();
+
+//Es7
+const mul=(a,b)=>console.log(a*b);
+mul(2,4);
