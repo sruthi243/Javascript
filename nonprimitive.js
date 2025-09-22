10de1fcb4395bb0e5d2f78ebc7fe73e55d79c86a
Sruthi <sruthisirigiri93@gmail.com>
2025-09-22 11:08:10 +0530

    changes

---
+++ nonprimitive.js
@@ -0,0 +1,9 @@
+array=[56,"sruthi",true,null]
+console.log(typeof array);
+data={
+    name:"sruthi";
+    age:"21";
+    dept:"cse";
+
+}
+console.log(typeof data);
--- primitive.js
+++ primitive.js
@@ -1,18 +1,3 @@
-// types of variables
-//var, let,const
-//var-global variable
-//let,const - local varibale
-
-
-
-var a = 67;
-var a = 90;
-let b = 68;
-const c = 78;
-
-console.log('value b is ${b}');
-console.log('value c is ${c}');
-console.log(a);
 //comment -
 
 //commit used to understand the code by the other developer
